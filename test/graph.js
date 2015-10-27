768c6beac3f6736fb46004c4e5ee575535451b5f
Maximilian Klein <dasmaxl@gmail.com>
2015-10-27 07:46:21 +0100

    (test) Add test sketch for a translator graph.

--- test/graph.js
+++ test/graph.js
@@ -2,10 +2,17 @@
 
 var expect = require('chai').expect
 
-var dtype = require('dynatype')
-var graph = require('../lib/graph')
+var dtypeGraph = require('../lib/graph')
 
 describe('Translator Graph', function () {
   it('Creates a graph for a list of translators', function () {
+    var dynatypes = {
+      descriptors: [{name: 'A'}, {name: 'B'}, {name: 'C'}],
+      translators: [{from: 'A', to: 'B'}, {from: 'C', to: 'B'}]
+    }
+    var graph = dtypeGraph(dynatypes)
+    expect(graph).to.be.an('object')
+    expect(graph.nodes).to.have.length(3)
+    expect(graph.edges).to.have.length(2)
   })
 })
